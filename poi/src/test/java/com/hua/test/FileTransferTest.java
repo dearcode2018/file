854049874a1e5d0cf51fe63a578adc56d259d873
qianye.zheng <>
2021-01-26 10:15:21 +0800

    no message

---
+++ poi/src/test/java/com/hua/test/FileTransferTest.java
@@ -0,0 +1,297 @@
+/**
+ * 描述: 
+ * FileTransferTest.java
+ * 
+ * @author qye.zheng
+ *  version 1.0
+ */
+package com.hua.test;
+
+//静态导入
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.hua.constant.GridConstant;
+import com.hua.util.ClassPathUtil;
+import com.hua.util.POIUtil;
+
+
+/**
+ * 描述: 
+ * 
+ * @author qye.zheng
+ * FileTransferTest
+ */
+//@DisplayName("测试类名称")
+//@Tag("测试类标签")
+//@Tags({@Tag("测试类标签1"), @Tag("测试类标签2")})
+public final class FileTransferTest extends BaseTest {
+
+	
+    /**
+     * 
+     * 描述: 
+     * @author qye.zheng
+     * 
+     */
+    //@DisplayName("test")
+    @Test
+    public void test2003To2007() {
+        try {
+            Workbook workbook2007 = POIUtil.createWorkbook(GridConstant.EXCEL_XLSX);
+            String path = ClassPathUtil.getClassPath("/questionnaire.xls");
+            
+            //cloneSheet(null, null, path);
+            
+            /*
+             * InputStream inputStream = new FileInputStream(path);
+             * Workbook workbook = POIUtil.buildWorkbook(GridConstant.EXCEL_XLS, inputStream);
+             * Sheet sheet = workbook.cloneSheet(0);
+             */
+            
+            
+        } catch (Exception e) {
+            log.error("test =====> ", e);
+        }
+    }
+    
+
+    public static void cloneSheet(File excelFile, String srcSheetName, String destSheetName) {
+        Workbook sheets = readExcelFromFile(excelFile);
+        int index = sheets.getSheetIndex(srcSheetName);
+        cloneSheet(excelFile, index, destSheetName);
+    }
+
+    public static void cloneSheet(File excelFile, Integer index, String destSheetName) {
+        Workbook sheets = readExcelFromFile(excelFile);
+        //克隆一个新的sheet
+        Sheet newSheet = sheets.cloneSheet(index);
+        int sheetIndex = sheets.getSheetIndex(newSheet);
+        sheets.setSheetName(sheetIndex, destSheetName);
+        try {
+            FileOutputStream out = new FileOutputStream(excelFile);
+            out.flush();
+            sheets.write(out);
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //读取excel
+    public static Workbook readExcelFromFile(File file) {
+        if (file == null) {
+            return null;
+        }
+        try {
+            return new XSSFWorkbook(new FileInputStream(file));
+        } catch (IOException e) {
+            throw new RuntimeException("文件解析失败");
+        }
+    }
+    
+    
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	//@DisplayName("test")
+	@Test
+	public void test() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("test =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testTemp")
+	@Test
+	public void testTemp() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testTemp=====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testCommon")
+	@Test
+	public void testCommon() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testCommon =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testSimple")
+	@Test
+	public void testSimple() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testSimple =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testBase")
+	@Test
+	public void testBase() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testBase =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: [每个测试-方法]开始之前运行
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("beforeMethod")
+	@Tag(" [每个测试-方法]结束之后运行")
+	@BeforeEach
+	public void beforeMethod() {
+		System.out.println("beforeMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: [每个测试-方法]结束之后运行
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("afterMethod")
+	@Tag(" [每个测试-方法]结束之后运行")
+	@AfterEach
+	public void afterMethod() {
+		System.out.println("afterMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: 测试忽略的方法
+	 * @author qye.zheng
+	 * 
+	 */
+	@Disabled
+	@DisplayName("ignoreMethod")
+	@Test
+	public void ignoreMethod() {
+		System.out.println("ignoreMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: 解决ide静态导入消除问题 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("noUse")
+	@Disabled("解决ide静态导入消除问题 ")
+	private void noUse() {
+		String expected = null;
+		String actual = null;
+		Object[] expecteds = null;
+		Object[] actuals = null;
+		String message = null;
+		
+		assertEquals(expected, actual);
+		assertEquals(message, expected, actual);
+		assertNotEquals(expected, actual);
+		assertNotEquals(message, expected, actual);
+		
+		assertArrayEquals(expecteds, actuals);
+		assertArrayEquals(expecteds, actuals, message);
+		
+		assertFalse(true);
+		assertTrue(true);
+		assertFalse(true, message);
+		assertTrue(true, message);
+		
+		assertSame(expecteds, actuals);
+		assertNotSame(expecteds, actuals);
+		assertSame(expecteds, actuals, message);
+		assertNotSame(expecteds, actuals, message);
+		
+		assertNull(actuals);
+		assertNotNull(actuals);
+		assertNull(actuals, message);
+		assertNotNull(actuals, message);
+		
+		fail();
+		fail("Not yet implemented");
+		
+		dynamicTest(null, null);
+		
+		assumeFalse(false);
+		assumeTrue(true);
+		assumingThat(true, null);
+	}
+
+}
